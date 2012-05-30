def769f91229458145d7b500836cb2e7f371aab8
Ben Hall <Ben@BenHall.me.uk>
2012-05-30 11:09:37 +0100

    Demos

---
+++ HelloWorldExamples/CSQuery/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CsQuery;
+using EasyHttp.Http;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void csquery()
+        {
+            var dom = CsQuery.Server.CreateFromUrl("https://www.google.com/#q=ASP.net&fp=476a205d5fa78915");
+            //Fails. Google loads search results as a second stage.
+            StringAssert.Contains("Get Started with ASP.NET & ASP.NET MVC : Official Microsoft Site", dom.Text()); 
+        }
+
+        [Test]
+        public void easyhttp()
+        {
+            var httpClient = new HttpClient();
+            var response = httpClient.Get("http://www.amazon.co.uk/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=Testing+ASP.net&x=0&y=0");
+            var dom = CsQuery.CQ.Create(response.RawText);
+            StringAssert.Contains("Ben Hall", dom.Find(".ptBrand").Text());
+        }
+
+        [Test]
+        public void easyhttp_json()
+        {
+            var httpClient = new HttpClient();
+            var response = httpClient.Get("http://search.twitter.com/search.json?q=prognet&&rpp=5&include_entities=true&result_type=mixed");
+            Assert.That(response.DynamicBody.results.Length > 0);
+        }
+    }
+}
---
+++ HelloWorldExamples/SeleniumWebDriver/Example1/Example1/GoogleTests_1Example.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace Example1
+{
+    [TestFixture]
+    public class GoogleTests_1Example
+    {
+        [Test]
+        public void Search_for_ASPnet_returns_aspnet_homepage_as_result() {
+            //IWebDriver driver = CreateFireFox();
+            //IWebDriver driver = CreateChrome();
+            IWebDriver driver = CreateIE();
+            driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30)); //Wait while searching for an element.
+
+            driver.Navigate().GoToUrl("http://www.google.co.uk");
+            IWebElement searchbox = driver.FindElement(By.Name("q"));
+
+            searchbox.SendKeys("ASP.net");
+            searchbox.SendKeys(Keys.Enter);
+
+            Assert.IsNotNull(driver.FindElement(By.LinkText("Get Started with ASP.NET & ASP.NET MVC : Official Microsoft Site")));
+        }
+
+        private static IWebDriver CreateFireFox()
+        {
+            //FirefoxProfile profile = new FirefoxProfile()
+            //profile.addExtension(....);
+            return new OpenQA.Selenium.Firefox.FirefoxDriver(new OpenQA.Selenium.Firefox.FirefoxProfile());
+        }
+
+        private static IWebDriver CreateChrome()
+        {
+            return new OpenQA.Selenium.Chrome.ChromeDriver(@"."); //Ensure chromedriver.exe is copied to the bin folder
+        }
+
+        private static IWebDriver CreateIE()
+        {
+            //DesiredCapabilities capabilities = DesiredCapabilities.InternetExplorer();
+            //capabilities.SetCapability("ignoreProtectedModeSettings", true);
+            return new OpenQA.Selenium.IE.InternetExplorerDriver();
+        }
+    }
+}
